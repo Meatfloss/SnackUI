d1d3c03b640938877dd83543c50bf69205831bee
meatfloss <stream@Xiaochuans-Air.fios-router.home>
2017-10-25 00:42:05 -0400

    try on table

--- src/views/snack/snack.vue
+++ src/views/snack/snack.vue
@@ -5,127 +5,23 @@
 <template>
     <div class="home-main">
 
-        <Row>
-            <Col span="4">
-            {{snacks}}
-            </Col>
-            <Col span="4">
-                <Row>
-                    <Card>
-                        <Row type="flex" class="user-infor">
-                            <Col span="8">
-                                <Row class-name="made-child-con-middle" type="flex" align="middle">
-                                    <img class="avator-img" :src="avatorPath" />
-                                </Row>
-                            </Col>
-                            <Col span="16" style="padding-left:6px;">
-                                <Row class-name="made-child-con-middle" type="flex" align="middle">
-                                    <div>
-                                        <b class="card-user-infor-name">Admin</b>
-                                        <p>super admin</p>
-                                    </div>
-                                </Row>
-                            </Col>
-                        </Row>
-                        <div class="line-gray"></div>
-                        <Row class="margin-top-8">
-                            <Col span="8"><p class="notwrap">上次登录时间:</p></Col>
-                            <Col span="16" class="padding-left-8">2017.09.12-13:32:20</Col>
-                        </Row>
-                        <Row class="margin-top-8">
-                            <Col span="8"><p class="notwrap">上次登录地点:</p></Col>
-                            <Col span="16" class="padding-left-8">北京</Col>
-                        </Row>
-                    </Card>
-                </Row>
-                <Row class="margin-top-10">
-                    <Card>
-                        <p slot="title" class="card-title">
-                            <Icon type="android-checkbox-outline"></Icon>
-                            待办事项
-                        </p>
-                        <a type="text" slot="extra" @click.prevent="addNewToDoItem">
-                            <Icon type="plus-round"></Icon>
-                        </a>
-                        <Modal
-                            v-model="showAddNewTodo"
-                            title="添加新的待办事项"
-                            @on-ok="addNew"
-                            @on-cancel="cancelAdd">
-                            <Row type="flex" justify="center">
-                                <Input v-model="newToDoItemValue" icon="compose" placeholder="请输入..." style="width: 300px" />
-                            </Row>
-                            <Row slot="footer">
-                                <Button type="text" @click="cancelAdd">取消</Button>
-                                <Button type="primary" @click="addNew">确定</Button>
-                            </Row>
-                        </Modal>
-                        <div class="to-do-list-con">
-                            <div v-for="(item, index) in toDoList" :key="index" class="to-do-item">
-                                <to-do-list-item :content="item.title"></to-do-list-item>
-                            </div>
-                        </div>
-                    </Card>
-                </Row>
-            </Col>
-            <Col span="16" class-name="padding-left-5">
-                <Row>
-                    <Col span="6">
-                        <infor-card
-                            id-name="user_created_count"
-                            :end-val="count.createUser"
-                            iconType="android-person-add"
-                            color="#2d8cf0"
-                            intro-text="今日新增用户"
-                        ></infor-card>
-                    </Col>
-                    <Col span="6" class-name="padding-left-5">
-                        <infor-card
-                            id-name="visit_count"
-                            :end-val="count.visit"
-                            iconType="ios-eye"
-                            color="#64d572"
-                            :iconSize="50"
-                            intro-text="今日浏览量"
-                        ></infor-card>
-                    </Col>
-                    <Col span="6" class-name="padding-left-5">
-                        <infor-card
-                            id-name="collection_count"
-                            :end-val="count.collection"
-                            iconType="upload"
-                            color="#ffd572"
-                            intro-text="今日数据采集量"
-                        ></infor-card>
-                    </Col>
-                    <Col span="6" class-name="padding-left-5">
-                        <infor-card
-                            id-name="transfer_count"
-                            :end-val="count.transfer"
-                            iconType="shuffle"
-                            color="#f25e43"
-                            intro-text="今日服务调用量"
-                        ></infor-card>
-                    </Col>
-                </Row>
-                <Row class="margin-top-10">
-                    <Card :padding="0">
-                        <p slot="title" class="card-title">
-                            <Icon type="map"></Icon>
-                            今日服务调用地理分布
-                        </p>
-                        <div class="map-con">
-                            <Col span="10">
-                                <map-data-table :cityData="cityData" height="281" :style-obj="{margin: '12px 0 0 11px'}"></map-data-table>
-                            </Col>
-                            <Col span="14" class="map-incon">
-                                <Row type="flex" justify="center" align="middle">
-                                    <home-map :city-data="cityData"></home-map>
-                                </Row>
-                            </Col>
-                        </div>
-                    </Card>
-                </Row>
+        <Row class="margin-top-10">
+            <Col :xs="24" :lg="12">
+            <!-- <Card>
+                            {{snacks}}
+            </Card> -->
+                <Card>
+                    <p slot="title">
+                        <Icon type="pinpoint"></Icon>
+                        小吃列表
+                    </p>
+                    <Row>
+                        <Input v-model="searchConName1" icon="search" @on-change="handleSearch1" placeholder="请输入姓名搜索..." style="width: 200px" />
+                    </Row>
+                    <Row class="margin-top-10 searchable-table-con1">
+                        <Table :columns="snackColumns" :data="snacks"></Table>
+                    </Row>
+                </Card>
             </Col>
         </Row>
         <Row class="margin-top-10">
@@ -178,102 +74,118 @@
 </template>
 
 <script>
-import cityData from './map-data/get-city-value.js';
-import homeMap from './components/map.vue';
-import dataSourcePie from './components/dataSourcePie.vue';
-import visiteVolume from './components/visiteVolume.vue';
-import serviceRequests from './components/serviceRequests.vue';
-import userFlow from './components/userFlow.vue';
-import countUp from './components/countUp.vue';
-import inforCard from './components/inforCard.vue';
-import mapDataTable from './components/mapDataTable.vue';
-import toDoListItem from './components/toDoListItem.vue';
-import util from '@/libs/util.js';
+import cityData from "./map-data/get-city-value.js";
+import homeMap from "./components/map.vue";
+import dataSourcePie from "./components/dataSourcePie.vue";
+import visiteVolume from "./components/visiteVolume.vue";
+import serviceRequests from "./components/serviceRequests.vue";
+import userFlow from "./components/userFlow.vue";
+import countUp from "./components/countUp.vue";
+import inforCard from "./components/inforCard.vue";
+import mapDataTable from "./components/mapDataTable.vue";
+import toDoListItem from "./components/toDoListItem.vue";
+import util from "@/libs/util.js";
 
 export default {
-    name: 'home',
-    components: {
-        homeMap,
-        dataSourcePie,
-        visiteVolume,
-        serviceRequests,
-        userFlow,
-        countUp,
-        inforCard,
-        mapDataTable,
-        toDoListItem
-    },
-    data () {
-        return {
-            toDoList: [
-                {
-                    title: '去iView官网学习完整的iView组件'
-                },
-                {
-                    title: '去iView官网学习完整的iView组件'
-                },
-                {
-                    title: '去iView官网学习完整的iView组件'
-                },
-                {
-                    title: '去iView官网学习完整的iView组件'
-                },
-                {
-                    title: '去iView官网学习完整的iView组件'
-                }
-            ],
-            count: {
-                createUser: 496,
-                visit: 3264,
-                collection: 24389305,
-                transfer: 39503498
-            },
-            cityData: cityData,
-            showAddNewTodo: false,
-            newToDoItemValue: '',
-            snacks: [],
-        };
-    },
-    computed: {
-        avatorPath () {
-            return localStorage.avatorImgPath;
+  name: "home",
+  components: {
+    homeMap,
+    dataSourcePie,
+    visiteVolume,
+    serviceRequests,
+    userFlow,
+    countUp,
+    inforCard,
+    mapDataTable,
+    toDoListItem
+  },
+  data() {
+    return {
+      toDoList: [
+        {
+          title: "去iView官网学习完整的iView组件"
+        },
+        {
+          title: "去iView官网学习完整的iView组件"
+        },
+        {
+          title: "去iView官网学习完整的iView组件"
+        },
+        {
+          title: "去iView官网学习完整的iView组件"
+        },
+        {
+          title: "去iView官网学习完整的iView组件"
         }
-    },
-    methods: {
-        getSnacks () {
-            util.ajax.get(`snacks`)
-                .then(response => {
-                    this.snacks = response.data
-                })
-            .catch(e => {
-              this.errors.push(e)
-            })
+      ],
+      count: {
+        createUser: 496,
+        visit: 3264,
+        collection: 24389305,
+        transfer: 39503498
+      },
+      cityData: cityData,
+      showAddNewTodo: false,
+      newToDoItemValue: "",
+      snacks: [],
+      snackColumns: [
+        {
+          key: "name",
+          title: "名字"
         },
-
-        addNewToDoItem () {
-            this.showAddNewTodo = true;
-        },
-        addNew () {
-            if (this.newToDoItemValue.length !== 0) {
-                this.toDoList.unshift({
-                    title: this.newToDoItemValue
-                });
-                setTimeout(function () {
-                    this.newToDoItemValue = '';
-                }, 200);
-                this.showAddNewTodo = false;
-            } else {
-                this.$Message.error('请输入待办事项内容');
-            }
-        },
-        cancelAdd () {
-            this.showAddNewTodo = false;
-            this.newToDoItemValue = '';
+        {
+          key: "image",
+          title: "图片链接"
         }
-
-    },
-    mounted () {
-        this.getSnacks();
+      ]
+    };
+  },
+  computed: {
+    avatorPath() {
+      return localStorage.avatorImgPath;
     }
+  },
+  methods: {
+    getSnacks() {
+      util.ajax
+        .get(`snacks`)
+        .then(response => {
+          //this.snacks = response.data;
+          for (var i = 0; i < response.data.length; i++) {
+            this.snacks.push({
+              name: response.data[i].name,
+              image: response.data[i].image_url
+            });
+          }
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+
+    addNewToDoItem() {
+      this.showAddNewTodo = true;
+    },
+    addNew() {
+      if (this.newToDoItemValue.length !== 0) {
+        this.toDoList.unshift({
+          title: this.newToDoItemValue
+        });
+        setTimeout(function() {
+          this.newToDoItemValue = "";
+        }, 200);
+        this.showAddNewTodo = false;
+      } else {
+        this.$Message.error("请输入待办事项内容");
+      }
+    },
+    cancelAdd() {
+      this.showAddNewTodo = false;
+      this.newToDoItemValue = "";
+    }
+  },
+  mounted() {
+    this.getSnacks();
+  }
 };
 </script>
